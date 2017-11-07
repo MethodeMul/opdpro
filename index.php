58436a8fe1703877697fc85fd71eda1207500a42
Meth <methodemul@outlook.com>
2017-11-07 15:46:52 +0300

    duplicate removed

--- index.php
+++ index.php
@@ -104,36 +104,5 @@ div.simpleContContent {
 </table>
 </div>
 <div id="er" color="444"><?php if(isset($er) && $er != '') echo $er; ?></div>
-
-<div id="simpleContContent" class="simpleContContent" style="opacity: 1;">
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#FFFFFF">
-<tr>
-<form name="form1" method="post" action="#">
-<td>
-<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-<tr>
-<td colspan="2" style="padding-bottom:15px; padding-top:15px;"><strong>Welcome to NKOZI Hospital Information Management tool</strong></td>
-</tr>
-<tr>
-<td colspan="2" align="center" style="padding-bottom:15px"><img src="images/doctortool2.jpg"></td>
-</tr>
-<tr>
-<td width="78">Username</td>
-<td width="294"><input name="myusername" type="text" id="myusername"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><input name="mypassword" type="password" id="mypassword"></td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td><input type="submit" name="Submit" value="Login"></td>
-</tr>
-</table>
-</td>
-</form>
-</tr>
-</table>
-</div>
-		
+	
 			
